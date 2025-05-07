4a6766b462eac31ef509b5e38d1d849ffa62a81c
ChrisGreenroom <Chris.greenroom@hotmail.com>
2025-05-07 19:43:30 +0200

    completed: worksheet_01_myData. Integrated into programm.

---
+++ includes/task_01_myData.h
@@ -0,0 +1,11 @@
+/* Task_01_myData:
+ * for full description of the task, see file:
+ * "worksheet_01_myData.pdf" in docs folder.
+ */
+
+#ifndef TASK_01_H
+#define TASK_01_H
+
+void print_personal_data(void);
+
+#endif
