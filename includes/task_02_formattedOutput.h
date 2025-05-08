d40d52d45452eabb69e619b558922c68d76027af
ChrisGreenroom <chris.greenroom@hotmail.com>
2025-05-08 18:47:14 +0200

    completed: task_02_formattedOutput.

---
+++ includes/task_02_formattedOutput.h
@@ -0,0 +1,11 @@
+/* Task_02_formattedOutput:
+ * for full description of the task, see file:
+ * "worksheet_02_formattedOutput.pdf" in docs folder.
+ */
+
+ #ifndef TASK_02_formattedOutput_H
+ #define TASK_02_formattedOutput_H
+ 
+ void print_formattedOutput(void);
+    
+ #endif
