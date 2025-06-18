479986789a2b04c30fef134f8037f8a7776de538
Braun <s240325@th-ab.de>
2025-06-18 12:04:14 +0200

    Task_09-Loops: started.

---
+++ includes/task_09-loops.h
@@ -0,0 +1,8 @@
+// task_09-loops
+
+#ifndef TASK_09-LOOPS_H
+#define TASK_09-LOOPS_H
+
+void small_101(void);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -2,12 +2,14 @@
 #include "tools.h"
 #include "task_01_myData.h"
 #include "task_02_formattedOutput.h"
+#include "task_09-loops.h"
 
 int main(void)
 {
-    say_hello();
-    print_personal_data();
-    print_formattedOutput();
+    //say_hello();
+    //print_personal_data();
+    //print_formattedOutput();
+    small_101();
 
     printf("\nThis is the End! \n");
     printf("Press any key to continue.\n");
